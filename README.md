74f390473950efb54348154cd3b74121579b994b
Louis Savoie <thelousavoie@gmail.com>
2020-11-09 19:09:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dance, Dance
+
+This is my first attemp at live coding music with Sonic Pi.
+
+I am recreating the song "Dance, Dance" by Fall Out Boy, starting with the iconic bass line by Pete Wentz, to test out how all of this works.
---
+++ danceDance.rb
@@ -0,0 +1,72 @@
+# "Dance, Dance" by Fall Out Boy
+# Bass lines by Pete Wentz
+
+#120bpm, 2bps
+#whole-note: 0.5
+#half-note: 0.25
+#quarter-note: 0.125
+#eight-note: 0.05125
+
+# Bass Riff 1
+
+play :B2, amp: 1, sustain: 0.125, release: 0.125
+sleep 0.25
+
+play :B2, amp: 0.5, sustain: 0, release: 0.05125
+sleep 0.125
+
+play :B2, amp: 0.5, sustain: 0.125, release: 0.125
+sleep 0.25
+
+play :B2, amp: 0.75, sustain: 0, release: 0.05125
+sleep 0.125
+
+play :D2, amp: 1, sustain: 0.625
+sleep 0.5
+
+play :D2, amp: 0.25, sustain: 0.125, release: 0.125
+sleep 0.25
+
+play :A2, amp: 0.5, release: 0.125
+sleep 0.125
+
+play :A2, amp: 0.5, release: 0.125
+sleep 0.125
+
+play :B2, amp: 0.25, release: 0.125
+sleep 0.125
+
+play :A2, amp: 0.5, release: 0.125
+sleep 0.125
+
+# Bass Riff 2
+
+play :G2, amp: 1, sustain: 0.125, release: 0.125
+sleep 0.25
+
+play :G2, amp: 0.5, sustain: 0, release: 0.05125
+sleep 0.125
+
+play :G2, amp: 0.5, sustain: 0.125, release: 0.125
+sleep 0.25
+
+play :G2, amp: 0.75, sustain: 0, release: 0.05125
+sleep 0.125
+
+play :D2, amp: 1, sustain: 0.625
+sleep 0.5
+
+play :D2, amp: 0.25, sustain: 0.125, release: 0.125
+sleep 0.25
+
+play :A2, amp: 0.5, release: 0.125
+sleep 0.125
+
+play :A2, amp: 0.5, release: 0.125
+sleep 0.125
+
+play :B2, amp: 0.25, release: 0.125
+sleep 0.125
+
+play :A2, amp: 0.5, release: 0.125
+sleep 0.125
